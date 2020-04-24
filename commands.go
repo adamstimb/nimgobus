325812fb8c219fbf8fe08300a0b44da16ef3896c
Tim Adams <adamstimb@gmail.com>
2020-04-24 13:14:28 +0200

    needs textscroll

--- commands.go
+++ commands.go
@@ -1,6 +1,8 @@
 package nimgobus
 
 import (
+	"fmt"
+
 	"github.com/hajimehoshi/ebiten"
 )
 
@@ -194,8 +196,43 @@ func (n *Nimbus) SetPen(c int) {
 // Put plots an ASCII char at the cursor position
 func (n *Nimbus) Put(c int) {
 	// todo: validate c
+	// Pick the textbox
+	box := n.textBoxes[n.selectedTextBox]
+	width := box.col2 - box.col1
+	height := box.row2 - box.row1
 	// Get x, y coordinate of cursor and draw the char
-	ex, ey := n.convertColRow(n.cursorPosition)
+	relCurPos := n.cursorPosition
+	var absCurPos colRow // we need the absolute cursor position
+	absCurPos.col = relCurPos.col + box.col1
+	absCurPos.row = relCurPos.row + box.row1
+	ex, ey := n.convertColRow(absCurPos)
+	ex -= 8
+	ey -= 10
+	fmt.Println(ex, ey)
+
 	// Draw the char
 	n.drawChar(n.paper, c, int(ex), int(ey), n.penColour)
+	// Update relative cursor position
+	relCurPos.col++
+	// Carriage return?
+	if relCurPos.col > width+1 {
+		// over the edge so carriage return
+		relCurPos.col = 1
+		relCurPos.row++
+	}
+	// Scroll up?
+	if relCurPos.row > height+1 {
+		// move cursor up and scroll textbox
+		relCurPos.row--
+		// scroll
+	}
+	// Set new cursor position
+	n.cursorPosition = relCurPos
+}
+
+// Print prints a string in the selected textbox
+func (n *Nimbus) Print(text string) {
+	for _, c := range text {
+		n.Put(int(c))
+	}
 }
