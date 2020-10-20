e63f5ce959feb618fb1073053632ff888670b74f
adamstimb <adamstimb@gmail.com>
2020-10-20 09:22:44 +0200

    removing package global vars

--- examples/greeting/greeting.go
+++ examples/greeting/greeting.go
@@ -13,47 +13,52 @@ const (
 	screenHeight = 1000
 )
 
-var (
-	nim nimgobus.Nimbus
-)
-
 type Game struct {
 	count int
+	nimgobus.Nimbus
 }
 
 func (g *Game) Update() error {
 	if g.count == 0 {
-		go Test()
+		go Test(g)
 	}
 	g.count++
-	nim.Update()
+	g.Nimbus.Update()
 	return nil
 }
 
-func Test() {
-	nim.Boot()       // Boot the Nimbus! (this is optional)
-	nim.SetMode(40)  // Low-res, high-colour mode
-	nim.SetBorder(9) // Light blue border
-	nim.SetPaper(1)  // Dark blue paper
-	nim.Cls()        // Clear screen
+func NewGame() *Game {
+	game := &Game{}
+
+	game.Init()
+
+	return game
+}
+
+func Test(g *Game) {
+	g.Boot()       // Boot the Nimbus! (this is optional)
+	g.SetMode(40)  // Low-res, high-colour mode
+	g.SetBorder(9) // Light blue border
+	g.SetPaper(1)  // Dark blue paper
+	g.Cls()        // Clear screen
 	// Plot some text with a shadow effect
 	op := nimgobus.PlotOptions{
 		SizeX: 3, SizeY: 6, Brush: 0,
 	}
-	nim.Plot(op, "Nimgobus", 65, 150)
+	g.Plot(op, "Nimgobus", 65, 150)
 	op.Brush = 14
-	nim.Plot(op, "Nimgobus", 67, 152)
+	g.Plot(op, "Nimgobus", 67, 152)
 	op.SizeX = 1
 	op.SizeY = 1
 	op.Brush = 13
-	nim.Plot(op, "it ain't no real thing", 70, 70)
-	nim.PlonkLogo(8, 8) // Draw the Nimbus BIOS logo
+	g.Plot(op, "it ain't no real thing", 70, 70)
+	g.PlonkLogo(8, 8) // Draw the Nimbus BIOS logo
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
 	// Draw the Nimbus monitor on the screen and scale to current window size.
-	monitorWidth, monitorHeight := nim.Monitor.Size()
+	monitorWidth, monitorHeight := g.Monitor.Size()
 	// Calculate aspect ratios of Nimbus monitor and ebiten screen
 	monitorRatio := float64(monitorWidth) / float64(monitorHeight)
 	screenRatio := float64(screenWidth) / float64(screenHeight)
@@ -77,7 +82,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	op.GeoM.Scale(scale, scale)
 	op.GeoM.Translate(offsetX, offsetY)
 	op.Filter = ebiten.FilterLinear
-	screen.DrawImage(nim.Monitor, op)
+	screen.DrawImage(g.Monitor, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -86,15 +91,14 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
 
-	// Initialize the Nimbus
-	nim.Init()
-
 	// set up window
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Nimgobus Test")
 
+	game := NewGame()
+
 	// Call RunGame method, passing the address of the pointer to an empty Game struct
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
