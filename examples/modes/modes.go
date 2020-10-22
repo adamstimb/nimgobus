e8ca32d159c71fc0f30d7a89799dc1468855ed5b
adamstimb <adamstimb@gmail.com>
2020-10-22 08:41:18 +0200

    examples updated

--- examples/modes/modes.go
+++ examples/modes/modes.go
@@ -9,47 +9,51 @@ import (
 	"github.com/hajimehoshi/ebiten"
 )
 
-const (
-	screenWidth  = 1400
-	screenHeight = 1000
-)
-
-var (
-	nim nimgobus.Nimbus
-)
-
 type Game struct {
-	count int
+	count           int
+	nimgobus.Nimbus // Embed the Nimbus in the Game struct
+}
+
+func NewGame() *Game {
+	game := &Game{}
+	game.Init() // Initialize Nimgobus
+	return game
 }
 
 func (g *Game) Update() error {
 	if g.count == 0 {
-		go Test()
+		go App(g) // Launch the Nimbus app on first iteration
 	}
 	g.count++
-	nim.Update()
+	g.Nimbus.Update() // Update the app on all subsequent iterations
 	return nil
 }
 
-func Test() {
-	Mode80()
-	time.Sleep(4 * time.Second)
-	Mode40()
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return outsideWidth, outsideHeight
 }
 
-func Mode80() {
-	nim.SetMode(80)
+func App(g *Game) {
+	// This is the Nimbus app itself
+	Mode80(g)
+	time.Sleep(4 * time.Second)
+	Mode40(g)
+}
+
+func Mode80(g *Game) {
+	// Show a colour swatch and info about mode 80
+	g.SetMode(80)
 	plotOpts := nimgobus.PlotOptions{
 		Brush: 2,
 		SizeX: 2,
 		SizeY: 2,
 	}
-	nim.Plot(plotOpts, "Mode 80", 30, 220)
+	g.Plot(plotOpts, "Mode 80", 30, 220)
 	plotOpts.Brush = 3
-	nim.Plot(plotOpts, "Mode 80", 32, 221)
-	nim.SetWriting(1, 5, 7, 75, 11)
-	nim.SetWriting(1)
-	nim.Print("This is Mode 80.  The screen is 80 character columns wide and 25 columns tall, or 640 pixels wide and 250 pixels tall.  Pixels are doubled in length along the vertical so everything has this wacky stretched-out look!  4 colours are available.")
+	g.Plot(plotOpts, "Mode 80", 32, 221)
+	g.SetWriting(1, 5, 7, 75, 11)
+	g.SetWriting(1)
+	g.Print("This is Mode 80.  The screen is 80 character columns wide and 25 columns tall, or 640 pixels wide and 250 pixels tall.  Pixels are doubled in length along the vertical so everything has this wacky stretched-out look!  4 colours are available.")
 	// Draw colour swatch
 	areaOpts := nimgobus.AreaOptions{}
 	var x, y int
@@ -59,28 +63,29 @@ func Mode80() {
 		y = 50
 		x = 30 + (i * width)
 		areaOpts.Brush = 3
-		nim.Area(areaOpts, x-1, y-1, x+width+1, y-1, x+width+1, y+81, x-1, y+81, x-1, y-1)
+		g.Area(areaOpts, x-1, y-1, x+width+1, y-1, x+width+1, y+81, x-1, y+81, x-1, y-1)
 		areaOpts.Brush = i
-		nim.Area(areaOpts, x, y, x+width, y, x+width, y+80, x, y+80, x, y)
+		g.Area(areaOpts, x, y, x+width, y, x+width, y+80, x, y+80, x, y)
 	}
 }
 
-func Mode40() {
-	nim.SetMode(40)
-	nim.SetPaper(1)
-	nim.SetBorder(1)
-	nim.Cls()
+func Mode40(g *Game) {
+	// Show a colour swatch and info about mode 40
+	g.SetMode(40)
+	g.SetPaper(1)
+	g.SetBorder(1)
+	g.Cls()
 	plotOpts := nimgobus.PlotOptions{
 		Brush: 0,
 		SizeX: 2,
 		SizeY: 2,
 	}
-	nim.Plot(plotOpts, "Mode 40", 15, 220)
+	g.Plot(plotOpts, "Mode 40", 15, 220)
 	plotOpts.Brush = 14
-	nim.Plot(plotOpts, "Mode 40", 16, 221)
-	nim.SetWriting(1, 3, 6, 38, 13)
-	nim.SetWriting(1)
-	nim.Print("This is Mode 40.  The screen is 40 character columns wide and 25 columns tall, or 320 pixels wide and 250 pixels tall.  16 sumptious colours are available.")
+	g.Plot(plotOpts, "Mode 40", 16, 221)
+	g.SetWriting(1, 3, 6, 38, 13)
+	g.SetWriting(1)
+	g.Print("This is Mode 40.  The screen is 40 character columns wide and 25 columns tall, or 320 pixels wide and 250 pixels tall.  16 sumptious colours are available.")
 	// Draw colour swatch
 	areaOpts := nimgobus.AreaOptions{}
 	var x, y int
@@ -90,32 +95,36 @@ func Mode40() {
 		y = 50
 		x = 15 + (i * width)
 		areaOpts.Brush = 15
-		nim.Area(areaOpts, x-1, y-1, x+width+1, y-1, x+width+1, y+81, x-1, y+81, x-1, y-1)
+		g.Area(areaOpts, x-1, y-1, x+width+1, y-1, x+width+1, y+81, x-1, y+81, x-1, y-1)
 		areaOpts.Brush = i
-		nim.Area(areaOpts, x, y, x+width, y, x+width, y+80, x, y+80, x, y)
+		g.Area(areaOpts, x, y, x+width, y, x+width, y+80, x, y+80, x, y)
 	}
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-
 	// Draw the Nimbus monitor on the screen and scale to current window size.
-	monitorWidth, monitorHeight := nim.Monitor.Size()
+	monitorWidth, monitorHeight := g.Monitor.Size()
+
+	// Get ebiten window size so we can scale the Nimbus screen up or down
+	// but if (0, 0) is returned we're not running on a desktop so don't do any scaling
+	windowWidth, windowHeight := ebiten.WindowSize()
+
 	// Calculate aspect ratios of Nimbus monitor and ebiten screen
 	monitorRatio := float64(monitorWidth) / float64(monitorHeight)
-	screenRatio := float64(screenWidth) / float64(screenHeight)
+	windowRatio := float64(windowWidth) / float64(windowHeight)
 
-	// If screenRatio > monitorRatio then clamp monitorHeight to screenHeight otherwise
+	// If windowRatio > monitorRatio then clamp monitorHeight to windowHeight otherwise
 	// clamp monitorWidth to screenWidth
 	var scale, offsetX, offsetY float64
-	if screenRatio > monitorRatio {
-		scale = float64(screenHeight) / float64(monitorHeight)
-		offsetX = (float64(screenWidth) - float64(monitorWidth)*scale) / 2
+	switch {
+	case windowRatio > monitorRatio:
+		scale = float64(windowHeight) / float64(monitorHeight)
+		offsetX = (float64(windowWidth) - float64(monitorWidth)*scale) / 2
 		offsetY = 0
-	}
-	if screenRatio <= monitorRatio {
-		scale = float64(screenWidth) / float64(monitorWidth)
+	case windowRatio <= monitorRatio:
+		scale = float64(windowWidth) / float64(monitorWidth)
 		offsetX = 0
-		offsetY = (float64(screenHeight) - float64(monitorHeight)*scale) / 2
+		offsetY = (float64(windowHeight) - float64(monitorHeight)*scale) / 2
 	}
 
 	// Apply scale and centre monitor on screen
@@ -123,24 +132,18 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	op.GeoM.Scale(scale, scale)
 	op.GeoM.Translate(offsetX, offsetY)
 	op.Filter = ebiten.FilterLinear
-	screen.DrawImage(nim.Monitor, op)
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return outsideWidth, outsideHeight
+	screen.DrawImage(g.Monitor, op)
 }
 
 func main() {
+	// Set up resizeable window
+	ebiten.SetWindowSize(1400, 1000)
+	ebiten.SetWindowTitle("Nimgobus")
+	ebiten.SetWindowResizable(true)
 
-	// Initialize the Nimbus
-	nim.Init()
-
-	// set up window
-	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Nimgobus Test")
-
-	// Call RunGame method, passing the address of the pointer to an empty Game struct
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	// Create a new game and pass it to RunGame method
+	game := NewGame()
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
